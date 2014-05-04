5e467f60300ac71e4894dbabe2c69f2a1095b433
Lucas Churchill <lucas@agtlucas.com>
2014-05-04 10:01:13 -0300

    Update NoneexistentEntityException

--- DeltaEJB-ejb/src/java/mi6/ejbs/exceptions/NoneexistentEntityException.java
+++ DeltaEJB-ejb/src/java/mi6/ejbs/exceptions/NoneexistentEntityException.java
@@ -11,5 +11,12 @@ package mi6.ejbs.exceptions;
  * @author AgtLucas
  */
 public class NoneexistentEntityException {
-    
+    public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+    }
 }
