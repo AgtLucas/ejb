8c5d11a65d61a84858ac92470b148ac327be8643
Lucas Churchill <lucas@agtlucas.com>
2014-05-03 23:03:25 -0300

    Adding UserRole Entity

---
+++ DeltaEJB-ejb/src/java/mi6/entity/UserRole.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mi6.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author AgtLucas
+ */
+
+@Entity
+public class UserRole implements Serializable {
+    
+}
