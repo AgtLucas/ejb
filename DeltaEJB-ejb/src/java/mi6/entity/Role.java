908411d6410759b04f0c821336aa49045defe1f3
Lucas Churchill <lucas@agtlucas.com>
2014-05-03 23:00:05 -0300

    Adding Role Entity

---
+++ DeltaEJB-ejb/src/java/mi6/entity/Role.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mi6.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author AgtLucas
+ */
+
+@Entity
+public class Role implements Serializable {
+    
+}
