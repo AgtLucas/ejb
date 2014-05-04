9e07a84cf2be9629331b28eb032203d83fda8561
Lucas Churchill <lucas@agtlucas.com>
2014-05-04 17:33:42 -0300

    Update

--- DeltaEJB-ejb/src/java/mi6/ejbs/UserFacade.java
+++ DeltaEJB-ejb/src/java/mi6/ejbs/UserFacade.java
@@ -6,9 +6,13 @@
 
 package mi6.ejbs;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import mi6.ejbs.exceptions.NoneexistentEntityException;
 import mi6.entity.User;
 
 /**
@@ -16,17 +20,54 @@ import mi6.entity.User;
  * @author AgtLucas
  */
 @Stateless
-public class UserFacade extends AbstractFacade<User> implements UserFacadeLocal {
+public class UserFacade /*extends AbstractFacade<User>*/ implements UserFacadeLocal, Serializable {
     @PersistenceContext(unitName = "DeltaEJB-ejbPU")
-    private EntityManager em;
+    
+    private EntityManagerFactory emf = null;
+//    private EntityManager em;
 
     @Override
-    protected EntityManager getEntityManager() {
-        return em;
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
 
-    public UserFacade() {
-        super(User.class);
+//    public UserFacade() {
+//        super(User.class);
+//    }
+
+    @Override
+    public void create(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit(User user) throws NoneexistentEntityException.NonexistentEntityException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(User user) throws NoneexistentEntityException.NonexistentEntityException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User find(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> findRange(int[] range) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int count() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
