63c9593b0eafe024ef150c20b591ddb878277f21
Lucas Churchill <lucas@agtlucas.com>
2014-05-07 13:23:10 -0300

    Go back button

--- DeltaEJB-war/src/java/mi6/controller/AddUserRoleServlet.java
+++ DeltaEJB-war/src/java/mi6/controller/AddUserRoleServlet.java
@@ -63,6 +63,7 @@ public class AddUserRoleServlet extends HttpServlet {
                 urfl.create(ur);
                 response.sendRedirect("listuser");
             }
+            out.println("<a class=\"go-back\" href=\"index.html\">Home</a>");
             out.println("</div>");
             out.println("</body>");
             out.println("</html>");
